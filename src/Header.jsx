c29a6629d9c85b8a0a9416b0ddf60135a511314e
Masum Jafar <masumjafar96@gmail.com>
2025-09-26 10:41:17 +0530

    update

--- src/Header.jsx
+++ src/Header.jsx
@@ -62,7 +62,7 @@ function Header(){
         <div className="header">
             <div className="headerLeft">
                 <Link to="/"><img className="header_icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Populaar</span></Link>
                 <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
                 <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
                 <Link to="/favourite" style={{textDecoration: "none"}}><span>Favourite</span></Link>
